61668197a5e8411b3341b234d0c55c6bfa9a6384
Junglesouljah <13595688964.@163.com>
2024-05-28 22:47:04 +0800

    首次提交

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, request
 from database import dataBaseManager
 
-
 app = Flask(__name__)
 
 c = dataBaseManager()
 
 
-@app.route('/<username>', methods=["GET"])
-def checkAccout(username):
-    return str(c.card_exists(username))
-
-
 @app.route('/register', methods=["POST"])
 def register():
     data = request.get_json()
-    print(data)
-    return c.register(username=data['username'], password=data['password'], account=data['account'])
+    if c.user_exists(data['account']) is not None:
+        return {"message": "账户已被注册","code":400}, 400
+    else:
+        return c.register(password=data['password'], account=data['account'])
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.get_json()
+    if c.user_exists(data['account']) is None:
+        return {"message": "账户未注册，请先注册账户后再登陆!","code":400}, 400
+    else:
+        return c.login(data['account'], data['password'])
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- database.py
+++ database.py
@@ -1,4 +1,7 @@
 import mysql.connector
+import uuid
+
+
 class dataBaseManager:
     HOST = '1.92.131.215'
     USERNAME = 'aiFlower'
@@ -13,7 +16,7 @@ class dataBaseManager:
             database=self.DATABASE
         )
 
-    def execute_query(self, sql, val=None):
+    def execute_query(self, sql, val=None, is_select=False):
         result = None
         error = None
         try:
@@ -24,29 +27,96 @@ class dataBaseManager:
                     cursor.execute(sql, val)
                 else:
                     cursor.execute(sql)
-                result = cursor.fetchone()
+
+                if is_select:
+                    result = cursor.fetchall()  # 获取查询结果
+                    if not result:
+                        result = None
+
+                else:
+                    result = cursor.rowcount  # 获取受影响的行数
+
                 self.db.commit()
         except mysql.connector.Error as e:
             error = str(e)
-        return result, error
 
-    def card_exists(self, username):
-        sql = "SELECT * FROM user WHERE username =%s"
-        val = (username,)
-        result, error = self.execute_query(sql, val)
+        return (result, error)  # 返回元组形式的结果和错误信息
+
+    def user_exists(self, userAccount):
+        sql = "SELECT * FROM user WHERE userAccount =%s"
+        val = (userAccount,)
+        result, error = self.execute_query(sql, val, is_select=True)
         return result
 
-    def register(self, username, account, password):
-        sql = "INSERT INTO user (username, userAccount, userPassword) VALUES (%s, %s, %s)"
-        val = (username, account, password)
-        result, error = self.execute_query( sql, val)
-        if error:
+    def collate_token(self, token):
+        sql = "SELECT * FROM user WHERE token =%s"
+        val = (token,)
+        result, error = self.execute_query(sql, val)
+    def getInfo(self,account):
+        sql = "SELECT * FROM user where userAccount = %s"
+        val = (account,)
+        result, error = self.execute_query(sql,val,is_select=True)
+        print(result)
+        return result
+    def register(self, account, password):
+        try:
+            sql = "INSERT INTO user ( userAccount, userPassword,token,updateTime,username) VALUES ( %s, %s,%s,NOW(),%s)"
+            token = uuid.uuid4().hex
+            name = f'用户{account}'
+            val = (account, password, token,name)
+            result, error = self.execute_query(sql, val, is_select=False)
+            if error:
+                return {
+                    'message': f'注册失败 {error}',
+                    'code': 400
+                }, 400
+            else:
+                if result >0:
+                    return {
+                        'message': '注册成功',
+                        'token': token,
+                        'code': 200,
+                        'name':name
+                    }, 200
+                else:
+                    return {
+                        'message': '注册失败',
+                        'code': 400
+                    }, 400
+        except Exception as e:
             return {
-                'message': '注册失败!!!，请联系管理园!',
-                'code': 401
-            }
-        else:
+                'message': '注册失败，发生异常: {}'.format(str(e)),
+                'code': 500
+            }, 500
+
+    def login(self, account, password):
+        try:
+            sql = "UPDATE user set token = %s WHERE userAccount = %s and userPassword = %s"
+            token = uuid.uuid4().hex
+            val = (token, account, password)
+            result, error = self.execute_query(sql, val, is_select=False)
+            if error:
+                return {
+                    'message': f'登录失败，{error}',
+                    'code': 400
+                }, 400
+            else:
+                if result > 0:  # 检查受影响的行数来判断登陆成功
+                    info = self.getInfo(account)[0]
+                    print(info)
+                    return {
+                        'message': '登录成功',
+                        'token': token,
+                        'name':info[1],
+                        'code': 200
+                    }, 200
+                else:  # 否则表示用户名或密码错误
+                    return {
+                        'message': '用户名或密码错误',
+                        'code': 400
+                    }, 400
+        except Exception as e:
             return {
-                'message': '注册成功',
-                'code': 200
-            }
+                'message': '登录失败，发生异常: {}'.format(str(e)),
+                'code': 500
+            }, 500
